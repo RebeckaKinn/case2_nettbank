86b7c718d0474d94171312f5662fd40ed16111b0
RebeckaKinn <rebeckaki@getacademy.no>
2022-12-28 11:59:04 +0100

    added images and started on the view

---
+++ js/view.js
@@ -0,0 +1,14 @@
+mainSite();
+function mainSite(){
+    model.site.innerHTML = /*HTML*/`
+    <div><img src="img/logo.png"></div>
+    <div class="menu">
+        <h3>MENY</h3>
+        <div>oversikt</div>
+        <div>eFaktura</div>
+        <div>budsjettkalkulator</div>
+        <div>pengebruk</div>
+        <div>instillinger</div>
+    </div>
+    `;
+}
