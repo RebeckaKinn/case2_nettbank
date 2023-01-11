a0adddd22adf6c5e7997f389b59968bb2e9f3fc7
RebeckaKinn <rebeckaki@getacademy.no>
2023-01-11 14:07:54 +0100

    Update view.js

--- js/view.js
+++ js/view.js
@@ -52,11 +52,12 @@ function mainSite(){
 
     <div class="meny">
         <h3>MENY</h3><br/>
-        <button class="menuButton">oversikt</button><br/>
-        <button class="menuButton">samlet oversikt</button><br/>
+        <button class="menuButton" onclick="mainSite()">oversikt</button><br/>
+        <button class="menuButton" onclick="overview()">samlet oversikt</button><br/>
         <button class="menuButton">budsjettkalkulator</button><br/>
         <button class="menuButton">pengebruk</button><br/>
-        <button class="menuButton">instillinger</button><br/>
+        <button class="menuButton">betale regninger</button><br/>
+        <button class="menuButton" onclick="settings()">instillinger</button><br/>
         <button class="menuButton">legg til konto</button><br/>
     </div>
 
@@ -96,5 +97,96 @@ function mainSite(){
 }
 
 function seeAllTransactions(){
+    model.site.innerHTML = /*HTML*/`
+    <div class="container">
+    <div class="icon"><img src="img/logo_mainsite.png" style=" height: 120px;"></div>
+    <div class="titleMainSite">Oversikt</div>
+    <div class="welcomeTitle">Velkommen</div><div class="nameTag">${model.loggedInUser[0].name} ${model.loggedInUser[0].lastname}</div>
+    <button class="logOutButton" onclick="logOut()">Logg ut</button>
+
+    <div class="meny">
+        <h3>MENY</h3><br/>
+        <button class="menuButton" onclick="mainSite()">oversikt</button><br/>
+        <button class="menuButton" onclick="overview()">samlet oversikt</button><br/>
+        <button class="menuButton">budsjettkalkulator</button><br/>
+        <button class="menuButton">pengebruk</button><br/>
+        <button class="menuButton">betale regninger</button><br/>
+        <button class="menuButton" onclick="settings()">instillinger</button><br/>
+        <button class="menuButton">legg til konto</button><br/>
+    </div>
+    <div class="">NAVN</div>
+    <div class="">SUM</div>
+    <div class="">DATO</div>
+
+    <div class="">NAVN</div>
+    <div class="">SUM</div>
+    <div class="">DATO</div>
+    `;
+    for(let i = 0; i < model.loggedInUser[0].uttak.length; i++){
+        model.site.innerHTML += /*HTML*/`
+            <div class="uttakNamei">${model.loggedInUser[0].uttak[i].name}</div>
+            <div class="uttakSumi">${model.loggedInUser[0].uttak[i].sum}</div>
+            <div class="uttakDatei">${model.loggedInUser[0].uttak[i].date}</div>
+            `;
+    }
+    for(let j = 0; j < model.loggedInUser[0].innskudd.length; j++){
+        model.site.innerHTML += /*HTML*/`
+            <div class="innskuddNamej">${model.loggedInUser[0].innskudd[j].name}</div>
+            <div class="innskuddSumj">${model.loggedInUser[0].innskudd[j].sum}</div>
+            <div class="innskuddDatej">${model.loggedInUser[0].innskudd[j].date}</div>
+        `;
+    }
+}
+
+function overview(){
+    model.site.innerHTML = /*HTML*/`
+    <div class="container">
+    <div class="icon"><img src="img/logo_mainsite.png" style=" height: 120px;"></div>
+    <div class="titleMainSite">Oversikt</div>
+    <div class="welcomeTitle">Velkommen</div><div class="nameTag">${model.loggedInUser[0].name} ${model.loggedInUser[0].lastname}</div>
+    <button class="logOutButton" onclick="logOut()">Logg ut</button>
+
+    <div class="meny">
+        <h3>MENY</h3><br/>
+        <button class="menuButton" onclick="mainSite()">oversikt</button><br/>
+        <button class="menuButton" onclick="overview()">samlet oversikt</button><br/>
+        <button class="menuButton">budsjettkalkulator</button><br/>
+        <button class="menuButton">pengebruk</button><br/>
+        <button class="menuButton">betale regninger</button><br/>
+        <button class="menuButton" onclick="settings()">instillinger</button><br/>
+        <button class="menuButton">legg til konto</button><br/>
+    </div>
+    <div class="">NAVN</div>
+    <div class="">SUM</div>
+    <div class="">DATO</div>
+    `;
+    for(let i = 0; i < model.loggedInUser[0].kontoer.length; i++){
+        model.site.innerHTML += /*HTML*/`
+            <div class="kontoNamei">${model.loggedInUser[0].kontoer[i].name}</div>
+            <div class="kontoSumi">${model.loggedInUser[0].kontoer[i].sum}</div>
+            <div class="kontoDatei">${model.loggedInUser[0].kontoer[i].date}</div>
+        `;
+    }
+}
+
+function settings(){
+    model.site.innerHTML = /*HTML*/`
+    <div class="container">
+    <div class="icon"><img src="img/logo_mainsite.png" style=" height: 120px;"></div>
+    <div class="titleMainSite">Oversikt</div>
+    <div class="welcomeTitle">Velkommen</div><div class="nameTag">${model.loggedInUser[0].name} ${model.loggedInUser[0].lastname}</div>
+    <button class="logOutButton" onclick="logOut()">Logg ut</button>
+
+    <div class="meny">
+        <h3>MENY</h3><br/>
+        <button class="menuButton" onclick="mainSite()">oversikt</button><br/>
+        <button class="menuButton" onclick="overview()">samlet oversikt</button><br/>
+        <button class="menuButton">budsjettkalkulator</button><br/>
+        <button class="menuButton">pengebruk</button><br/>
+        <button class="menuButton">betale regninger</button><br/>
+        <button class="menuButton" onclick="settings()">instillinger</button><br/>
+        <button class="menuButton">legg til konto</button><br/>
+    </div>
     
+    `;
 }
